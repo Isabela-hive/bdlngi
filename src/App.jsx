c72bdb69953235afd26aecbbb3670ffc995effe5
DevWanzy <wanzyisabel@gmail.com>
2023-05-07 08:46:59 +0300

    more commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import "./App.css";
-import React, { Component } from "react";
-import Splash from "./components/Splash";
-import Navbar from "./components/Navbar";
-import { gsap } from "gsap";
+import './App.css';
+import React, { Component } from 'react';
+import Splash from './components/Splash';
+import Navbar from './components/Navbar';
+import { gsap } from 'gsap';
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   NavLink,
-} from "react-router-dom";
-import Home from "./pages/Home";
-import c from "./constants";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { AnimatePresence } from "framer-motion/dist/framer-motion";
-import Footer from "./components/Footer";
-import Travel from "./pages/Travel";
-import History from "./pages/History";
-import Stories from "./pages/Stories";
+} from 'react-router-dom';
+import Home from './pages/Home';
+import c from './constants';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { AnimatePresence } from 'framer-motion/dist/framer-motion';
+import Footer from './components/Footer';
+import Travel from './pages/Travel';
+import History from './pages/History';
+import Stories from './pages/Stories';
 
 class App extends Component {
   constructor() {
@@ -36,13 +36,13 @@ class App extends Component {
 
   async closeMenu(e) {
     if (e.keyCode === 27) {
-      let p = document.querySelectorAll(".navbar .navitem");
+      let p = document.querySelectorAll('.navbar .navitem');
       for (var i of p) {
         await gsap.fromTo(i, 0.1, { opacity: 1, x: 0 }, { opacity: 0, x: 50 });
       }
-      gsap.to(".navbar", { backdropFilter: "blur(0px)" });
-      gsap.to(".navbar > *", 0.6, { opacity: 0 }).then(() => {
-        gsap.to(".nv", 0.6, { bottom: "100vh" });
+      gsap.to('.navbar', { backdropFilter: 'blur(0px)' });
+      gsap.to('.navbar > *', 0.6, { opacity: 0 }).then(() => {
+        gsap.to('.nv', 0.6, { bottom: '100vh' });
       });
     }
   }
@@ -56,7 +56,7 @@ class App extends Component {
           ) : (
             <div
               onClick={() => {
-                window.addEventListener("keyup", this.closeMenu);
+                window.addEventListener('keyup', this.closeMenu);
                 //if (!this.state.navOpen) {
 
                 // } else {
@@ -66,32 +66,32 @@ class App extends Component {
             >
               <div
                 style={{
-                  position: "fixed",
-                  padding: "2vw 5vw",
+                  position: 'fixed',
+                  padding: '2vw 5vw',
                   zIndex: 1,
-                  display: "flex",
-                  alignItems: "center",
-                  flexDirection: "row",
+                  display: 'flex',
+                  alignItems: 'center',
+                  flexDirection: 'row',
                   top: 0,
                   left: 0,
                   right: 0,
                 }}
               >
                 <FontAwesomeIcon
-                  style={{ cursor: "pointer" }}
+                  style={{ cursor: 'pointer' }}
                   icon={faBars}
                   size="2x"
                   onClick={() => {
                     gsap
-                      .to(".nv", 0.6, {
-                        bottom: "0vh",
+                      .to('.nv', 0.6, {
+                        bottom: '0vh',
                       })
                       .then(async () => {
-                        gsap.to(".navbar", 1, { backdropFilter: "blur(10px)" });
-                        gsap.to(".navbar > *", 0.6, {
+                        gsap.to('.navbar', 1, { backdropFilter: 'blur(10px)' });
+                        gsap.to('.navbar > *', 0.6, {
                           opacity: 1,
                         });
-                        let p = document.querySelectorAll(".navbar .navitem");
+                        let p = document.querySelectorAll('.navbar .navitem');
                         for (var i of p) {
                           // let cc = i.classList.contains('activenav')
                           await gsap.fromTo(
@@ -137,11 +137,11 @@ class App extends Component {
               this.setState((p) => (p.routes = r));
             }}
             closeMe={async () => {
-              window.removeEventListener("keyup", this.closeMenu);
+              window.removeEventListener('keyup', this.closeMenu);
               // if (!this.state.navOpen) {
               //   gsap.to('.nv', 1, { bottom: '0vh' })
               // } else {
-              let p = document.querySelectorAll(".navbar .navitem");
+              let p = document.querySelectorAll('.navbar .navitem');
               for (var i of p) {
                 await gsap.fromTo(
                   i,
@@ -150,9 +150,9 @@ class App extends Component {
                   { opacity: 0, x: 50 }
                 );
               }
-              gsap.to(".navbar", { backdropFilter: "blur(0px)" });
-              gsap.to(".navbar > *", 0.6, { opacity: 0 }).then(() => {
-                gsap.to(".nv", 0.6, { bottom: "100vh" });
+              gsap.to('.navbar', { backdropFilter: 'blur(0px)' });
+              gsap.to('.navbar > *', 0.6, { opacity: 0 }).then(() => {
+                gsap.to('.nv', 0.6, { bottom: '100vh' });
               });
               // }
             }}
