2fb83b90454c24efe0e8e84278cdc8da1d8d75da
DevWanzy <wanzyisabel@gmail.com>
2023-05-07 17:11:21 +0300

    modified

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,26 +3,24 @@ import Carousel from '../components/Carousel';
 import Inspiration from '../components/Inspiration';
 import { motion } from 'framer-motion/dist/framer-motion';
 import Contributors from '../components/Contributors';
+import '../pages/styles/home.css';
 
 function Home() {
   const [bg, setBg] = useState('');
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 1, delay: 0, easing: 'ease-in-out' }}
-    >
-      <p>
-        Hey Isabellah Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Libero perferendis error natus dolor. Officia voluptatum facere itaque
-        nobis quidem unde.
-      </p>
-      <Carousel setBg={(b) => setBg(b)} />
-      <Inspiration bg={bg} />
-      <Contributors />
-    </motion.div>
+    <div className="people">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1, delay: 0, easing: 'ease-in-out' }}
+      >
+        <Carousel setBg={(b) => setBg(b)} />
+        <Inspiration bg={bg} />
+        <Contributors />
+      </motion.div>
+    </div>
   );
 }
 
