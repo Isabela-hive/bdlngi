c3fbe39484696de6e84d7ce155ee7745d99a0372
DevWanzy <wanzyisabel@gmail.com>
2023-05-07 09:21:20 +0300

    yeah

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,20 +6,20 @@ import Contributors from '../components/Contributors';
 
 function Home() {
   const [bg, setBg] = useState('');
-  <div className="home">
-    return (
+
+  return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 1, delay: 0, easing: 'ease-in-out' }}
     >
+      <p>Hey Isabellah</p>
       <Carousel setBg={(b) => setBg(b)} />
       <Inspiration bg={bg} />
       <Contributors />
     </motion.div>
-    );
-  </div>;
+  );
 }
 
 export default Home;
